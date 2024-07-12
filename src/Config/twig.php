1652fc1bc6ba1d2e04e5b156f794490609044266
Asko Nõmm <asko@nth.ee>
2024-07-12 22:59:31 +0300

    Organize

--- src/Config/twig.php
+++ src/Config/twig.php
@@ -3,19 +3,26 @@
 use Asko\Wptf\Template;
 
 return [
-    'get_field' => [Template::class, 'get_field'],
-    'get_fields' => [Template::class, 'get_fields'],
-    'language_attributes' => [Template::class, 'language_attributes'],
-    'bloginfo' => [Template::class, 'bloginfo'],
+    // WPTF
     'tailwind' => [Template::class, 'tailwind'],
     'url' => [Template::class, 'url'],
     'asset' => [Template::class, 'asset'],
+
+    // ACF
+    'get_field' => [Template::class, 'get_field'],
+    'get_fields' => [Template::class, 'get_fields'],
+
+    // WordPress
+    'language_attributes' => [Template::class, 'language_attributes'],
+    'bloginfo' => [Template::class, 'bloginfo'],
     'wp_nav_menu' => [Template::class, 'wp_nav_menu'],
-    'language_switcher' => [Template::class, 'wpml_language_switcher'],
-    'current_language' => [Template::class, 'wpml_current_language'],
     'the_content' => [Template::class, 'the_content'],
     '__' => [Template::class, '__'],
     'wp_title' => [Template::class, 'wp_title'],
     'wp_head' => [Template::class, 'wp_head'],
     'wp_footer' => [Template::class, 'wp_footer'],
+
+    // WPML
+    'wpml_language_switcher' => [Template::class, 'wpml_language_switcher'],
+    'wpml_current_language' => [Template::class, 'wpml_current_language'],
 ];
